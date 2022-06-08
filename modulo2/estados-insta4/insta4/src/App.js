52a8e124f8d2e3d3e13aca0f31ffe15ca0e1d736
Brenno Boechat <BrennoBoechat81@hotmail.com>
2022-06-08 17:31:13 -0300

    EXERCICIO DE COMPONENTES DE CLASSE + STATE

---
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+        <Post
+          nomeUsuario={'Brenno'}
+          fotoUsuario={'https://picsum.photos/50/50?random=1'}
+          fotoPost={'https://picsum.photos/200/150?random=1'}        
+        />
+        <Post
+          nomeUsuario={'Carlos'}
+          fotoUsuario={'https://picsum.photos/seed/picsum/50/50'}
+          fotoPost={'https://picsum.photos/id/237/200/150'} 
+          />    
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
