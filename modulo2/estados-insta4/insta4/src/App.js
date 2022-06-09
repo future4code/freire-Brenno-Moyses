a817ac3dc7701f859efb0c4a8ccfb4e03f8dc053
Brenno Boechat <BrennoBoechat81@hotmail.com>
2022-06-09 16:56:15 -0300

    inicio do exercicio de renderização de listas

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -10,24 +10,40 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+  state = {
+    posts: [
+      {
+      nomeUsuario: "paulinha",
+      fotoUsuario: "https://picsum.photos/50/50",
+      fotoPost: "https://picsum.photos/200/150"
+      },
+      {
+      nomeUsuario: "Brenno",
+      fotoUsuario: "https://picsum.photos/50/50?random=1",
+      fotoPost: "https://picsum.photos/200/150?random=1"
+      },
+      {
+      nomeUsuario: "Carlos",
+      fotoUsuario: "https://picsum.photos/seed/picsum/50/50",
+      fotoPost: "https://picsum.photos/200/150?random=1"
+      }
+  ]
+  };
   render() {
+    const postList = this.state.posts.map((usuario, index) =>{
+      return (
+        <div key={index}>
+          <Post
+            nomeUsuario={usuario.nomeUsuario}
+            fotoUsuario={usuario.fotoUsuario}
+            fotoPost={usuario.fotoPost}
+            />
+        </div>)
+    }
+    )
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'Brenno'}
-          fotoUsuario={'https://picsum.photos/50/50?random=1'}
-          fotoPost={'https://picsum.photos/200/150?random=1'}        
-        />
-        <Post
-          nomeUsuario={'Carlos'}
-          fotoUsuario={'https://picsum.photos/seed/picsum/50/50'}
-          fotoPost={'https://picsum.photos/id/237/200/150'} 
-          />    
+        {postList}
       </MainContainer>
     );
   }
