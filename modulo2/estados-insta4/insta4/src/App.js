9d85cb20a81622aec663056bd2911b80ec5d7617
Brenno Boechat <BrennoBoechat81@hotmail.com>
2022-06-09 18:55:06 -0300

    exercicio de renderização de lista completo

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -6,7 +6,19 @@ const MainContainer = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
-  align-items: center;
+  align-items: center;`
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  
+  input{
+    margin: 10px;
+    width: 250px;
+    height: 25px;
+  }`
+const Button = styled.button`
+    margin: 10px;
+    padding: 10px 40px;
 `
 
 class App extends React.Component {
@@ -27,8 +39,32 @@ class App extends React.Component {
       fotoUsuario: "https://picsum.photos/seed/picsum/50/50",
       fotoPost: "https://picsum.photos/200/150?random=1"
       }
-  ]
+    ],
+      valueInputUser: "",
+      valueInputUserPhoto: "",
+      valueInputPost: "",
   };
+
+  addPost = () => {
+    const newPost = {
+      nomeUsuario: this.state.valueInputUser,
+      fotoUsuario: this.state.valueInputUserPhoto,
+      fotoPost: this.state.valueInputPost
+    };
+    const newPosts = [...this.state.posts, newPost];
+    this.setState({posts: newPosts});
+  };
+
+  onChangeInputUser = (event) => {
+    this.setState({valueInputUser: event.target.value});
+  };
+  onChangeInputUserPhoto = (event) => {
+    this.setState({valueInputUserPhoto: event.target.value});
+  };
+  onChangeInputPost = (event) => {
+    this.setState({valueInputPost: event.target.value});
+  };
+  
   render() {
     const postList = this.state.posts.map((usuario, index) =>{
       return (
@@ -43,10 +79,30 @@ class App extends React.Component {
     )
     return (
       <MainContainer>
+        <Form>
+        <input
+          value={this.state.valueInputUser}
+          onChange={this.onChangeInputUser}
+          placeholder={"Usuario"}
+        />
+        <input
+          value={this.state.valueInputUserPhoto}
+          onChange={this.onChangeInputUserPhoto}
+          placeholder={"Foto do Usuario"}
+        />
+        <input
+          value={this.state.valueInputPost}
+          onChange={this.onChangeInputPost}
+          placeholder={"Post"}
+        />
+        </Form>
+        <Button onClick={this.addPost}>Postar</Button>
         {postList}
       </MainContainer>
-    );
+    )
+    
   }
 }
+
 
 export default App;
