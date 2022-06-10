9ed189bc22f0e5e2a4a1b55d017667bffc88201f
Brenno Boechat <BrennoBoechat81@hotmail.com>
2022-06-10 19:28:13 -0300

    estrutura e estilizacao do whatslab

--- modulo2/projeto-whatslab/whatslab/src/components/Chat.js
+++ modulo2/projeto-whatslab/whatslab/src/components/Chat.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 const ChatContainer = styled.div`
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
     flex-direction: columns;
     align-items: flex-end;
 
@@ -11,18 +11,49 @@ const ChatContainer = styled.div`
     max-width: 35vw;
     height: 100vh;
     margin: 0 auto;
+    background-image: url(https://i.pinimg.com/originals/35/f3/e9/35f3e9c4b86568b4919949a9307da2a9.png);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
 `
-const UserInput = styled.input`
-    padding: 4px 5px;
+const UserInputName = styled.input`
+    width: 200px;
+    height: 30px;
+    justify-content: center;
+    font-size: 20px;
+    border-radius: 20px 20px;
+    padding: 5px 10px;
+
+    ::placeholder{
+        padding: 0px 5px;
+    }
+`
+const UserInputMessage = styled.input`
+    width: 500px;
+    height: 30px;
+    font-size: 20px;
+    border-radius: 20px 20px;
+    padding: 5px 10px;
+
+    ::placeholder{
+        padding: 0px 5px;
+    }
+`
+const SendButton = styled.button`
+    width: 100px;
+    height: 43px;
+    font-size: 20px;
+    background-color: transparent;
+    border: none;
 `
 
 export default class Chat extends Component{
     render() {
         return(
             <ChatContainer>
-                <UserInput></UserInput>
-                <UserInput></UserInput>
-                <button></button>
+                <UserInputName placeholder="Nome"></UserInputName>
+                <UserInputMessage placeholder="Mensagem"></UserInputMessage>
+                <SendButton type="button"><img src="https://cdn-icons-png.flaticon.com/512/2983/2983788.png" height="45" width="50"/></SendButton>
             </ChatContainer>
         );
     }
