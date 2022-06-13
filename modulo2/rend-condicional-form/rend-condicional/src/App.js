ab1b700b67b737065d9b4994b36f7730e6b3fbfb
Brenno Boechat <BrennoBoechat81@hotmail.com>
2022-06-13 17:07:39 -0300

    estrutura feita

---
+++ modulo2/rend-condicional-form/rend-condicional/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from "react";
+import Stage1 from './components/Stage1';
+import Stage2 from './components/Stage2';
+import Stage3 from './components/Stage3';
+import Stage4 from './components/Stage4';
+
+function App() {
+  return (
+    <div className="App">
+      <Stage1></Stage1>
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo2/rend-condicional-form/rend-condicional/src/components/Stage1.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+export default class Stage1 extends React.Component{
+    render() {
+        return (
+            <h1>ETAPA 1 - DADOS GERAIS</h1> 
+        );
+    }
+}
