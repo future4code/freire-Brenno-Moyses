e5a3c2be48926df2ffb68991e1708c4a6b5f3fcd
Brenno Boechat <BrennoBoechat81@hotmail.com>
2022-07-05 16:54:51 -0300

    exercicio feito

---
+++ modulo3/use-effect/src/components/PokeCard/PokeCard.js
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
+const PokeCard = (props) => {
+  const [pokemon,setPokemon] = useState([]);
+
+  useEffect(() => {
+    pegaPokemon(props.pokemon);
+  },[props.pokemon]);
+  
+
+  const pegaPokemon = (PokemonName) => {
+    axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${PokemonName}`)
+        .then((res) => {
+            setPokemon(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+    return (
+      <div>
+        <p>{pokemon.name}</p>
+        <p>{pokemon.weight} Kg</p>
+        {pokemon.types && <p>{pokemon.types[0].type.name}</p>}
+        {pokemon.sprites && (
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+        )}
+      </div>
+    );
+}
+
+export default PokeCard;
