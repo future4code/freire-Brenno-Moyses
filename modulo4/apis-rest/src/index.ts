704c5edf15e6efd2a6eb612344c3f57c59dbc4c1
Brenno Boechat <BrennoBoechat81@hotmail.com>
2022-08-18 19:31:00 -0300

    exercicio finalizado

--- modulo4/apis-rest/src/index.ts
+++ modulo4/apis-rest/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import {usersList} from "./data";
+import {usersList, user} from "./data";
+import { createIndexedAccessTypeNode } from "typescript";
 
 const app = express();
 
@@ -11,7 +12,7 @@ app.listen(3003, () => {
     console.log("Server is running in http://localhost:3003")
 });
 
-app.get("/users", (req, res) => {
+app.get("/allUsers", (req, res) => {
     const getAllUsers = usersList.flat(1)
     res.status(200).send(getAllUsers)
 })
@@ -30,4 +31,53 @@ app.get("/users/:type", (req, res) => {
 // a) Passei por meio de path params
 
 // b) Usando o ENUM
+
+app.get("/users", (req, res) => {
+    const userName: string = req.query.name as string
+    try{
+        const filterName: user[] = usersList.filter((user) =>
+            user.name === userName
+        )
+        if(!filterName.length) {
+            res.statusCode = 401;
+            throw new Error ("Nenhum usuário encontrado.")
+        }
+        res.status(200).send(filterName)
+    }catch (error: any) {
+        res.status(res.statusCode || 500).send({ message: error.message})
+    }
+    
+})
+
+app.post("/createUser", (req, res) => {
+    const {name, email, type, age} = req.body
+    const newList = {
+        id: Math.floor(Math.random() * 100),
+        name: name,
+        email: email,
+        type: type,
+        age: age
+    }
+    usersList.push(newList)
+    res.status(200).send(usersList)
+})
+
+app.put("/changeUser", (req, res) => {
+    const {name, email, type, age} = req.body
+    const newList = {
+        id: Math.floor(Math.random() * 100),
+        name: name,
+        email: email,
+        type: type,
+        age: age
+    }
+    usersList.push(newList)
+    res.status(200).send(usersList)
+})
+
+// a) Mudou o objetivo do método, o PUT serve para atualizar
+
+// b) Sim pois ele muda algum item já existente, caso não exista ele cria.
+
+
 
