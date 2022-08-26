7952f2f0449d69757b075a7059f599a88705821a
Brenno Boechat <BrennoBoechat81@hotmail.com>
2022-08-26 00:05:07 -0300

    projeto iniciado

---
+++ modulo4/ToDoList/src/index.ts
@@ -0,0 +1,45 @@
+import knex from "knex";
+import app from "./app";
+import connection from "./connection";
+
+
+app.post('/user', async (req, res) => {
+    try{
+        await connection("User")
+        .insert({
+            id: Date.now(),
+            nickname: req.body.nickname,
+            email: req.body.email,
+            name: req.body.name
+        })
+        res.status(200).send("Usuário criado com sucesso!")
+    }catch (error : any){
+        res.status(res.statusCode || 500).send({ message: error.message})
+    }
+})
+
+app.get('/user/:id', async (req, res) => {
+    try{
+            const result = await connection("User")
+            .select("id", "nickname")
+            .from("User")
+            .where({id: req.params.id});
+        res.status(200).send(result)
+    }catch (error : any){
+        res.status(res.statusCode || 500).send({ message: error.message})
+    }
+})
+
+app.put('/user/edit/:id', async (req, res) => {
+    try{
+        await connection("User")
+        .update({
+            name: req.body.name,
+            nickname: req.body.nickname
+        })
+        .where({id:+req.params.id})
+        res.status(200).send("Editado com sucesso")
+    }catch (error : any){
+        res.status(res.statusCode || 500).send({ message: error.message})
+    }
+})
