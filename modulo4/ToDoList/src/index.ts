9d36f0a7fea7e03b66e15a1c6fe49c4bd6725870
Brenno Boechat <BrennoBoechat81@hotmail.com>
2022-08-28 00:34:55 -0300

    projeto finalizado

--- modulo4/ToDoList/src/index.ts
+++ modulo4/ToDoList/src/index.ts
@@ -1,10 +1,20 @@
 import knex from "knex";
 import app from "./app";
 import connection from "./connection";
+import moment from 'moment';
 
 
 app.post('/user', async (req, res) => {
     try{
+        if(
+            !req.body.name ||
+            !req.body.email ||
+            !req.body.nickname
+        ) {
+        res
+            .status (400)
+            .send("Preencha todos os campos")
+        }
         await connection("User")
         .insert({
             id: Date.now(),
@@ -32,6 +42,14 @@ app.get('/user/:id', async (req, res) => {
 
 app.put('/user/edit/:id', async (req, res) => {
     try{
+        if(
+            !req.body.name ||
+            !req.body.nickname  
+        ) {
+        res
+            .status (400)
+            .send("Preencha todos os campos")
+        }
         await connection("User")
         .update({
             name: req.body.name,
@@ -45,7 +63,14 @@ app.put('/user/edit/:id', async (req, res) => {
 })
 
 app.post('/task', async (req, res) => {
-    try{
+    try{ if (
+        !req.body.description ||
+        !req.body.title ||
+        !req.body.limit_date ||
+        !req.body.creator_user_id
+    ) {
+
+    }
         await connection("TodoListTask")
         .insert({
             id: Date.now().toString(),
@@ -60,3 +85,26 @@ app.post('/task', async (req, res) => {
         res.status(res.statusCode || 500).send({ message: error.message})
     }
 })
+
+app.get('/task/:id', async (req, res) => {
+    try{
+        const result = await connection("TodoListTask")
+        .where("TodoListTask.id", req.params.id)
+        .join("User", "TodoListTask.creator_user_id", "User.id")
+        .select("TodoListTask.id", "title", "description", "limit_date", "status", "User.nickname")
+
+        const formatDate = moment(result[0].limit_date, 'YYYY-MM-DD').format('DD/MM/YYYY')
+
+        res.status(200).send({
+            id: result[0].id,
+            title: result[0].title,
+            description: result[0].description,
+            limit_date: formatDate,
+            status: result[0].status,
+            nickname: result[0].nickname,
+        })
+    }catch (error : any){
+        res.status(res.statusCode || 500).send({ message: error.message})
+    }
+})
+
