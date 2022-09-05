ac138956d7c8dad24edf834e8d78648bbac6de62
Brenno Boechat <BrennoBoechat81@hotmail.com>
2022-09-05 19:01:39 -0300

    exercicio

---
+++ modulo4/classes-encapsulamento/src/index.ts
@@ -0,0 +1,84 @@
+// EXERCICIO 1
+
+//    a) Construtores são funções de inicialização de classe.
+//    Para chamar o construtor, precisamos declarar o método constructor e dentro os argumentos que a classe vai receber.
+
+        // type Transaction = {
+        //     description: string,
+        //     value: number,
+        //     date: string
+        // }
+
+        class UserAccount {
+            private cpf: string;
+            private name: string;
+            private age: number;
+            private balance: number = 0;
+            private transactions: Transaction[] = [];
+    
+        constructor(
+            cpf: string,
+            name: string,
+            age: number,
+        ) {
+        console.log("Chamando o construtor da classe UserAccount")
+            this.cpf = cpf;
+            this.name = name;
+            this.age = age;
+        }
+
+        public setTransactions(
+            newTransaction:Transaction
+        ):void{
+            this.transactions.push(newTransaction)
+        }
+
+        public getTransactions():Transaction[]{
+            return this.transactions
+        }
+
+    }
+
+        const newUser = new UserAccount("126.689.887-43", "Brenno", 22)
+
+    // b) Apenas uma vez.
+
+    // c) Podemos acessar usando a keyword this.
+
+    // EXERCICIO 2
+
+    class Transaction {
+        private date: string;
+        private value: number;
+        private description: string;
+
+    constructor(date: string, value: number, description: string){
+        this.date = date;
+        this.value = value;
+        this.description = description
+    }
+
+    public getDate(): string {
+        return this.date
+    }
+
+    public getValue(): number {
+        return this.value
+    }
+
+    public getDescription(): string {
+        return this.description
+    }
+} 
+
+    const newTransaction = new Transaction("23/03/2000", 1400, "Conta de luz")
+
+    newUser.setTransactions(newTransaction)
+    console.table(newUser.getTransactions())
+    console.table(newUser)
+
+    // EXERCICIO 3
+
+
+
+
