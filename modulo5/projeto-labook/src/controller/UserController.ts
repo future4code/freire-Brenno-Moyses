fe157e85357b784756942b72de1c3a872f89339a
Brenno Boechat <BrennoBoechat81@hotmail.com>
2022-09-22 16:53:55 -0300

    primeiro endpoint criado

---
+++ modulo5/projeto-labook/src/controller/UserController.ts
@@ -0,0 +1,26 @@
+import { UserBusiness } from "../business/UserBusiness";
+import { Response, Request } from "express";
+import { ISignupInputDTO } from "../models/User";
+
+export class UserController {
+    constructor(
+        private userBusiness: UserBusiness
+    ) {}
+
+    public signup = async (req: Request, res: Response) => {
+        try{
+            const input: ISignupInputDTO = {
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            }
+
+            const response = await this.userBusiness.signup(input)
+
+            res.status(201).send(response)
+        }catch(error: any){
+            res.status(400).send({message: error.message})
+        }
+    }
+
+}
