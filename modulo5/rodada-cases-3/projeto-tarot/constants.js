9bd0e4e7ee148ce721ded9f126e1979eaa6b30de
Brenno Boechat <BrennoBoechat81@hotmail.com>
2022-10-17 16:54:24 -0300

    :tada: Commit inicial

---
+++ modulo5/rodada-cases-3/projeto-tarot/constants.js
@@ -0,0 +1,1 @@
+export const BASE_URL = "https://raw.githubusercontent.com/Personare/front-end-challenge/master/tarot.json"
---
+++ modulo5/rodada-cases-3/projeto-tarot/src/App.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import {useEffect, useState} from "react"
+
+function App() {
+
+
+  return (
+    <div>
+      <p>OI</p>
+    </div>
+  );
+}
+
+export default App;
