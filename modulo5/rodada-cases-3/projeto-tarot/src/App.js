3481db42cd01ff0bff36be4c4b698001cce459e6
Brenno Boechat <BrennoBoechat81@hotmail.com>
2022-10-18 17:26:57 -0300

    :sparkles: feat: Renderização das cartas

--- modulo5/rodada-cases-3/projeto-tarot/src/App.js
+++ modulo5/rodada-cases-3/projeto-tarot/src/App.js
@@ -1,12 +1,36 @@
 import axios from "axios";
-import {useEffect, useState} from "react"
+import { BASE_URL } from "./constants";
+import { useEffect, useState } from "react"
+import { GlobalStyle } from "./Global";
 
 function App() {
+  const [cards, setCards] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}`)
+      .then((res) => {
+        setCards(res.data);
+        console.log(res.data)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      });
+  }, [])
+
+  // const ImageBackCard = cards.imageBackCard
+
+  const ImagesUrl = cards.imagesUrl;
+
+  const MappedCards = cards.cards?.map((card) => {
+      const renderCard = ImagesUrl+card.image
+      return <img src={renderCard}></img>
+  })
 
   return (
     <div>
-      <p>OI</p>
+      {MappedCards}
+      <GlobalStyle/>
     </div>
   );
 }
