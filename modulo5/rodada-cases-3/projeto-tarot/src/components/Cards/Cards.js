05220e5fcf2020ab4de433dae309923acae109c0
Brenno Boechat <BrennoBoechat81@hotmail.com>
2022-10-20 16:31:32 -0300

    :sparkles: feat: Embaralhar cartas

--- modulo5/rodada-cases-3/projeto-tarot/src/components/Cards/Cards.js
+++ modulo5/rodada-cases-3/projeto-tarot/src/components/Cards/Cards.js
@@ -24,8 +24,19 @@ function Cards(props) {
         
     })
 
+    function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+    }
+
+    console.log(cards)
+
     const flipCard = () => {
         setImageCard(!imageCard)
+        shuffleArray(cards.cards)
     }
 
 
